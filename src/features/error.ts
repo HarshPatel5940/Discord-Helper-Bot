45b136e041820bdb63b64dc1642a7b6c09798b5f
HARSH PATEL <83941233+HarshPatel5940@users.noreply.github.com>
2022-08-19 14:05:19 +0530

    Added Error Handling
    I should really make it better smh

---
+++ src/features/error.ts
@@ -0,0 +1,74 @@
+/**
+                Copyright [2022] [HarshPatel5940]
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License. */
+import { Client, MessageEmbed } from "discord.js";
+import dotenv from "dotenv";
+
+dotenv.config({ path: ".env.test" });
+
+export default (client: Client) => {
+    var title: string = "something went wrong";
+    var desc: any = "> *unknown error messsage*";
+
+    process.on("unhandledRejection", (reason) => {
+        title = "UNHANDLED REJECTION FOUND!!!";
+        desc = reason;
+        ReportError(client, title, desc);
+    });
+
+    process.on("uncaughtExceptionMonitor", (reason) => {
+        title = "UNCAUGHT EXCEPTION FOUND!!!";
+        desc = reason;
+        ReportError(client, title, desc);
+    });
+
+    process.on("multipleResolves", (type, reason) => {
+        title = "MULTIPLE RESOLVES FOUND!!!";
+        desc = `${reason}\n\ntype:${type}`;
+        ReportError(client, title, desc);
+    });
+};
+
+export const config = {
+    dbName: "INTERACTION_HANDLING",
+    displayName: "Error Handling",
+};
+
+function ReportError(client: Client, title: string, desc: string) {
+    const ErrEmbed = new MessageEmbed()
+        .setTitle(title)
+        .setDescription(
+            `
+\`\`\`
+${desc}
+\`\`\`
+Error Reported at: <t:${parseInt((Date.now() / 1000).toString())}:F>
+`
+        )
+        .setColor("RED")
+        .setFooter({ text: "Error Handling System." })
+        .setTimestamp();
+
+    console.log(title, "\n", desc, "\n\n");
+
+    if (!process.env.OWNER_ID) {
+        return;
+    }
+    const c = client.users.cache.get(process.env.OWNER_ID);
+    if (!c) {
+        return;
+    }
+
+    c.send({ embeds: [ErrEmbed] });
+}
